3eabc5545fa3d0be02aa94fd5581396efffdca84
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-07-10 19:17:49 -0300

    ajuste final

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
-}
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else {
+    //alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
+
+}
 const listaDeTeclas = document.querySelectorAll('.tecla'); // retorna uma nodeList
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
@@ -14,16 +22,20 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tocaSom(idAudio); // Chama a função tocaSom com o ID do elemento de áudio
   }
 
-  tecla.onkeydown = function(){
-    tecla.classList.add('ativa')
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+
   }
 
-  tecla.onkeyup = function(){
-    tecla.classList.remove('ativa')
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
   }
 }
 
-//Se você tiver acesso a este elemento via JavaScript e usar element.classList, obterá uma coleção de classes que inclui "tecla" e "tecla_pom". Portanto, element.classList[1] retornará "tecla_pom". 
+//Se você tiver acesso a este elemento via JavaScript e usar element.classList, obterá uma coleção de classes que inclui "tecla" e "tecla_pom". Portanto, element.classList[1] retornará "tecla_pom".
 
 // listaDeTeclas[contador] -> se refere a cada tag que possui a class .tecla -> itera cada uma
 
